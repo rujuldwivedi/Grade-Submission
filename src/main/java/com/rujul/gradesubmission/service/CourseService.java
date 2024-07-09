c28a11306573cfae87c14adb6a7b3a9688c2ad92
Rujul Dwivedi <rujuldwivedi@icloud.com>
2024-07-09 20:52:01 +0530

    Update and rename CourseService.java to CourseService.java

--- src/main/java/com/rujul/gradesubmission/service/CourseService.java
+++ src/main/java/com/rujul/gradesubmission/service/CourseService.java
@@ -1,8 +1,8 @@
-package com.ltp.gradesubmission.service;
+package com.rujul.gradesubmission.service;
 
 import java.util.List;
 
-import com.ltp.gradesubmission.entity.Course;
+import com.rujul.gradesubmission.entity.Course;
 
 public interface CourseService {
     Course getCourse(Long id);
