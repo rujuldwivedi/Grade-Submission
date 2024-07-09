871065f68960a79f583aac5ab67c999285f79d54
Rujul Dwivedi <rujuldwivedi@icloud.com>
2024-07-09 20:53:15 +0530

    Update and rename GradeService.java to GradeService.java

--- src/main/java/com/rujul/gradesubmission/service/GradeService.java
+++ src/main/java/com/rujul/gradesubmission/service/GradeService.java
@@ -1,8 +1,8 @@
-package com.ltp.gradesubmission.service;
+package com.rujul.gradesubmission.service;
 
 import java.util.List;
 
-import com.ltp.gradesubmission.entity.Grade;
+import com.rujul.gradesubmission.entity.Grade;
 
 public interface GradeService {
     Grade getGrade(Long studentId, Long courseId);
