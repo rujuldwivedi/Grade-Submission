7394695e3be080c78fdb930dd2d81da005fc56a0
Rujul Dwivedi <rujuldwivedi@icloud.com>
2024-07-09 20:47:43 +0530

    Update and rename CourseRepository.java to CourseRepository.java

--- src/main/java/com/rujul/gradesubmission/repository/CourseRepository.java
+++ src/main/java/com/rujul/gradesubmission/repository/CourseRepository.java
@@ -1,8 +1,8 @@
-package com.ltp.gradesubmission.repository;
+package com.rujul.gradesubmission.repository;
 
 import org.springframework.data.repository.CrudRepository;
 
-import com.ltp.gradesubmission.entity.Course;
+import com.rujul.gradesubmission.entity.Course;
 
 
 public interface CourseRepository extends CrudRepository<Course, Long> {
