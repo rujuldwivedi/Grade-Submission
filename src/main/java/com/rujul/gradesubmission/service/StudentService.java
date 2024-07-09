8ed80b0fab7a449c209fa4bd09e0e56b700c8a8e
Rujul Dwivedi <rujuldwivedi@icloud.com>
2024-07-09 20:54:13 +0530

    Update and rename StudentService.java to StudentService.java

--- src/main/java/com/rujul/gradesubmission/service/StudentService.java
+++ src/main/java/com/rujul/gradesubmission/service/StudentService.java
@@ -1,8 +1,8 @@
-package com.ltp.gradesubmission.service;
+package com.rujul.gradesubmission.service;
 
 import java.util.List;
 
-import com.ltp.gradesubmission.entity.Student;
+import com.rujul.gradesubmission.entity.Student;
 
 public interface StudentService {
     Student getStudent(Long id);
